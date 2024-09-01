92348455a527833f136110061213b4e172ea0b9a
tkatkimann <eaa23tkat@students.eaaa.dk>
2024-09-01 19:02:25 +0200

    added the codes

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+function onBulb() {
+    const bulb = document.getElementById("bulb");
+    bulb.src = "img/on.jpg";
+  }
+  
+  function offBulb() {
+    const bulb = document.getElementById("bulb");
+    bulb.src = "img/off.jpg";
+  }
+  
